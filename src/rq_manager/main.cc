ed1594b2f6645fe5f2a6f790faafba39255d0060
Paul Nieleck <paul.nieleck@tum.de>
2016-08-09 16:25:52 +0200

    Added 'libc.lib.so libm.lib.so stdcxx.lib.so' to runscripts build_boot_image - now the rq_manager runs

--- src/rq_manager/main.cc
+++ src/rq_manager/main.cc
@@ -5,48 +5,44 @@
  *
  */
 
+#include <base/env.h> /* not sure if needed */
 #include <base/printf.h>
-#include "rq_manager/rq_manager.h"
+//#include "rq_manager/rq_manager.h"
 #include "rq_manager/rq_buffer.h"
 
 // using namespace Rq_manager;
 
-int Rq_manager::_set_ncores(int n)
-{
-	_num_cores = n;
-	
-	return 0;
-}
-
-//	int _set_rqs(std::vector<Genode::fifo<int>> *r)
-//	{
-//		
-//	}
-
-Rq_manager::Rq_manager()
-{
-	PINF("Value of available system cores not provided -> set to 2.");
-
-	_set_ncores(2);
-}
-
-Rq_manager::Rq_manager(int num_cores)
-{
-	_set_ncores(num_cores);
-}
+//int Rq_manager::_set_ncores(int n)
+//{
+//	_num_cores = n;
+//	
+//	return 0;
+//}
+//
+//Rq_manager::Rq_manager()
+//{
+//	PINF("Value of available system cores not provided -> set to 2.");
+//
+//	_set_ncores(2);
+//}
+//
+//Rq_manager::Rq_manager(int num_cores)
+//{
+//	_set_ncores(num_cores);
+//}
 
 
 using namespace Genode;
 
-int main(void)
+int main()
 {
 
 	Rq_buffer<double> buf(10);
-	PWRN("New Buffer created");
+	PDBG("New Buffer created");
 	buf.enq(11.0);
 	buf.enq(22.0);
 	buf.enq(33.0);
-	PWRN("3 Elements enqueued");
+	PDBG("Elements enqueued");
 //	buf.deq();
 //	buf.deq();
 //	buf.deq();
